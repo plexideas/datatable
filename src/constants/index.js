7fe2c99b362fb5d240adbe241c555f977a84c0f0
8bitago <eightbitago@gmail.com>
2020-06-14 19:02:05 +0200

    Added constants

---
+++ src/constants/index.js
@@ -0,0 +1,19 @@
+export const TYPE = {
+  STRING: 'STRING',
+  NUMERIC: 'NUMERIC',
+  DATE: 'DATE',
+  TIMESTAMP: 'TIMESTAMP'
+}
+
+export const COLUMN = {
+  VALUE_1: 'VALUE_1',
+  CLIENT_NM: 'CLIENT_NM'
+}
+
+export const COLOR = {
+  LIGHT_BLUE: '#d8eefd',
+  BLUE: '#5aa9e6',
+  WHITE: '#f9f9f9',
+  RED: '#f50049',
+  YELLOW: '#ffe45e',
+}
