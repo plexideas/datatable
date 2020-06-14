2a0a243ec369fae3c79cb281f80bea662ec12d98
8bitago <eightbitago@gmail.com>
2020-06-15 00:10:13 +0200

    Added commonReducer and acrion for set editeble key

---
+++ src/reducers/commonReducer.js
@@ -0,0 +1,19 @@
+
+import { 
+  ACTION_COMMON_SET_EDITING_KEY,
+} from '../actions/commonActions';
+
+const initialState = {
+  editingKey: '',
+}
+
+const commonReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ACTION_COMMON_SET_EDITING_KEY: 
+      return {...state, editingKey: action.payload }
+    default: 
+      return state;
+  }
+}
+
+export default commonReducer;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 
 import dataReducer from './dataReducer';
 import columnsInfoReducer from './columnsInfoReducer';
+import commonReducer from './commonReducer';
 
 export default combineReducers({
   data: dataReducer,
-  columnsInfo: columnsInfoReducer
+  columnsInfo: columnsInfoReducer,
+  common: commonReducer
 });
