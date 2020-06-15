3bdc494f810fccf596171b3f742822e05ccfc3ef
8bitago <eightbitago@gmail.com>
2020-06-15 14:45:19 +0200

    A liitle fix TerminationDateInput

--- src/components/TerminationDateInput/TerminationDateInput.jsx
+++ src/components/TerminationDateInput/TerminationDateInput.jsx
@@ -5,18 +5,19 @@ import moment from 'moment';
 const TerminationDateInput = (props) => {
   const initDate = props.value 
     ? moment(new Date(props.value)) 
-    : moment(new Date())
+    : ''
   const [date, setDate] = useState(initDate);
   const handleOnChange = (value) => {
-    value && props.onChange && props.onChange(value.format('YYYY-MM'));
+    if (props.onChange) {
+      props.onChange(value ? value.format('YYYY-MM') : '');
+    }
     setDate(value)
   }
 
   return (
     <DatePicker.MonthPicker 
       value={date}
-      onChange={handleOnChange} 
-      defaultValue={moment(new Date())}
+      onChange={handleOnChange}
     />
   )
 }
